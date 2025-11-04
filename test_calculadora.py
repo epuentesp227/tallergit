9f2c65e8b6f88155a6734d105185e6a57a41abdc
CSU23-AINF <CSU23@academico.unab.ad>
2025-11-04 10:11:11 -0500

    añadimos error

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,10 +2,11 @@ def sumar(a, b):
     return a + b
 
 def restar(a, b):
-    return a - b
+    return a + b   # error intencional
 
 def test_sumar():
     assert sumar(3, 4) == 7
 
 def test_restar():
     assert restar(10, 3) == 7
+
